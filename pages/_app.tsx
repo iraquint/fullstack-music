4a073034492a7fa7282d49d2b06bbcbb5ba3a32d
Ira Quint <ira.quint@outschool.com>
2022-01-02 10:32:46 -0500

    step-2

---
+++ pages/_app.tsx
@@ -0,0 +1,42 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+// Reset CSS to be the same in every browser. All browsers apply CSS differently.
+import "reset-css";
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      // From Material Design gray pallette
+      100: "#F5F5F5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+});
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
