ee3d4d4691684fa438141a2c55b090737689addf
Ira Quint <ira.quint@outschool.com>
2022-01-17 09:27:13 -0500

    signup and signin

---
+++ components/authForm.tsx
@@ -0,0 +1,25 @@
+import { Box, Flex, Input, Button } from "@chakra-ui/layout";
+import { useRouter } from "next/router";
+import { useSWRConfig } from "swr";
+import { auth } from "../lib/mutations";
+import { FC, useState } from "react";
+
+const AuthForm: FC<{ mode: string }> = ({ mode }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  return (
+    <Box height='100vh' width='100vw' bg='white'>
+      <Flex justify="center" align='center' height='100px'>
+        hello
+      </Flex>
+      <Flex justify="center" align='center' height='calc(100vh - 100px)'>
+        form
+      </Flex>
+    </Box>
+  )
+};
+
+export default AuthForm;
